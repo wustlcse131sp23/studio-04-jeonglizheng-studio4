9a796c58b27484c6045025827c8ea9747793bb11
86139 <86139@Yaxin-Li.dhcp.wustl.edu>
2023-02-23 14:13:59 -0600

    Studio 4 Finished!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.clear();
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		StdDraw.setPenColor(120, 120, 0);
+		StdDraw.filledRectangle(3, 3, 3, 3);
+		StdDraw.filledRectangle(3, 13, 3, 3);
+		StdDraw.filledRectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(16, 13, 6, 3);
+		StdDraw.setPenColor(0, 0, 127);
+		StdDraw.filledRectangle(11, 8, 11, 1);
+		StdDraw.filledRectangle(8, 8, 1, 8);
 	}
 }
--- src/studio4/NorwayMotherOfAllFlags.java
+++ src/studio4/NorwayMotherOfAllFlags.java
@@ -31,31 +31,49 @@ public class NorwayMotherOfAllFlags {
 	public static void france() {
 		
 		// TODO
+		StdDraw.setXscale(8, 11);
+		StdDraw.setYscale(0, 2);
+		mother();
 	}
 
 	public static void indonesia() {
 		
 		// TODO
+		StdDraw.setXscale(0, 6);
+		StdDraw.setYscale(9, 11);
+		mother();
 	}
 
 	public static void netherlands() {
 		
 		// TODO
+		StdDraw.setXscale(0, 6);
+		StdDraw.setYscale(8, 11);
+		mother();
 	}
 
 	public static void poland() {
 		
 		// TODO
+		StdDraw.setXscale(0, 6);
+		StdDraw.setYscale(5, 7);
+		mother();
 	}
 
 	public static void thailand() {
 		
 		// TODO
+		StdDraw.setXscale(0, 6);
+		StdDraw.setYscale(5, 11);
+		mother();
 	}
 
 	public static void finland() {
 		
 		// TODO
+		StdDraw.setXscale(5, 15);
+		StdDraw.setYscale(6, 10);
+		mother();
 	}
 
 	public static void main(String[] args) {
